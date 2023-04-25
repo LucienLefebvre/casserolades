754a40c13100d29d0940bbc64c9ba1b993148dae
LucienLefebvre <lucien.lefebvre57@gmail.com>
2023-04-25 17:49:54 +0200

    base commit

---
+++ src/components/AboutWindow.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-card>
+    <div>
+      <h6 class="text-center text-red">Des casseroles pour Macron !!!</h6>
+      <p>
+        Comment ça marche ?
+        <br />
+        <br />
+        <b>Deux modes :</b><br />
+
+        <i>- Mode "Casserolade"</i><br />
+        Cliquez sur le bouton "générer" pour lancer une casserolade aléatoire.
+        Un appui long sur une case permet de régler les paramètres de chaque
+        casserole. <br />
+        <i>- Mode "Notes"</i><br />
+        Cliquez sur la note dans la bar du haut pour y accéder. <br />
+        Cliquez sur une case pour jouer une note. La séléction du son de
+        casserole se fait avec le menu déroulant en haut de l'écran
+      </p>
+    </div>
+  </q-card>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
